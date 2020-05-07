6210ca57fee84d1cda02d9bf3688df9d23f1a836
sayantan01 <cavi4762@gmail.com>
2020-05-07 10:32:10 +0530

    modified port

--- backend/src/index.js
+++ backend/src/index.js
@@ -32,4 +32,4 @@ app.get('/Library',(req,res)=>{
 })
 
 const PORT=process.env.PORT || 4000
-app.listen(4000,()=>console.log(`server up on port ${PORT}`))
+app.listen(PORT,()=>console.log(`server up on port ${PORT}`))
